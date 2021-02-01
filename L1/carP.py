ac20d24e530d227a059e66145b80fa51260cc0cb
lantiandelong <34814611+lantiandelong@users.noreply.github.com>
2021-02-01 16:47:54 +0800

    Update carP.py

--- L1/carP.py
+++ L1/carP.py
@@ -19,7 +19,9 @@ ptags=df.columns[7:]
 bresult2 = df.groupby(['car_model'])[ptags].agg('sum')
 bresult2 =bresult.merge(bresult2,left_index=True,right_index=True,how='left')
 bresult2 = bresult2.sort_values('count',ascending = False)
+print(bresult2)#输出按照车型统计的问题总数
 bresult3 = bresult2.sort_values('A14',ascending = False)
 bresult3.reset_index(inplace=True)
 bresult3= bresult3[['car_model','A14']]
+print(bresult3)#输出问题A14的车型排序
 #print(bresult3)
